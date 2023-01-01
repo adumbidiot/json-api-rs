4eea22387fcde367c79cc4ef2661177ba82aeb82
adumbidiot <nathaniel.daniel23@outlook.com>
2022-12-31 18:43:15 -0800

    Move client to new file

---
+++ src/client.rs
@@ -0,0 +1,37 @@
+use crate::Error;
+use crate::JsonDocument;
+use reqwest::header::HeaderValue;
+
+/// A Json:Api Client
+#[derive(Default)]
+pub struct Client {
+    client: reqwest::Client,
+}
+
+impl Client {
+    /// Make a new Client
+    pub fn new() -> Self {
+        Self {
+            client: reqwest::Client::new(),
+        }
+    }
+
+    /// Get a json document
+    pub async fn get_json_document<D>(&self, url: &str) -> Result<JsonDocument<D>, Error>
+    where
+        D: serde::de::DeserializeOwned,
+    {
+        static ACCEPT_HEADER_VALUE: HeaderValue =
+            HeaderValue::from_static("application/vnd.api+json");
+
+        let response = self
+            .client
+            .get(url)
+            .header(reqwest::header::ACCEPT, ACCEPT_HEADER_VALUE.clone())
+            .send()
+            .await?
+            .error_for_status()?;
+
+        Ok(response.json().await?)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! A basic client api for [JSON API](https://jsonapi.org/).
 
+mod client;
 mod types;
 
+pub use crate::client::Client;
 pub use crate::types::JsonDocument;
 pub use crate::types::LinksObject;
 pub use crate::types::ResourceObject;
@@ -12,35 +14,6 @@ pub enum Error {
     /// Reqwest HTTP error
     #[error(transparent)]
     Reqwest(#[from] reqwest::Error),
-}
-
-/// A Json:Api Client
-#[derive(Default)]
-pub struct Client {
-    client: reqwest::Client,
-}
-
-impl Client {
-    pub fn new() -> Self {
-        Self {
-            client: reqwest::Client::new(),
-        }
-    }
-
-    pub async fn get_json_document<D: serde::de::DeserializeOwned>(
-        &self,
-        url: &str,
-    ) -> Result<JsonDocument<D>, Error> {
-        let res = self
-            .client
-            .get(url)
-            .header(reqwest::header::ACCEPT, "application/vnd.api+json")
-            .send()
-            .await?
-            .error_for_status()?;
-
-        Ok(res.json().await?)
-    }
 }
 
 #[cfg(test)]

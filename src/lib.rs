27dd1e4a6af5ee4ff8f0fc3a472c3076ca09074f
Nathaniel Daniel <nathaniel.daniel23@outlook.com>
2022-12-31 18:54:17 -0800

    Cleanup (#24)
    * Update dependabot.yml
    
    * Create rustdoc action
    
    * Update CI
    
    * Allow Cargo.lock on git
    
    * Update deps
    
    * Fix deny
    
    * Update error type
    
    * Move client to new file
    
    * Allow tls backend to be configured

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,19 @@
 //! A basic client api for [JSON API](https://jsonapi.org/).
 
-pub mod types;
+mod client;
+mod types;
 
-pub use crate::types::{
-    JsonDocument,
-    LinksObject,
-    ResourceObject,
-};
+pub use crate::client::Client;
+pub use crate::types::JsonDocument;
+pub use crate::types::LinksObject;
+pub use crate::types::ResourceObject;
 
-pub type JsonResult<T> = Result<T, JsonError>;
-
-#[derive(Debug)]
-pub enum JsonError {
-    Reqwest(reqwest::Error),
-    InvalidStatus(reqwest::StatusCode),
-}
-
-impl From<reqwest::Error> for JsonError {
-    fn from(e: reqwest::Error) -> JsonError {
-        Self::Reqwest(e)
-    }
-}
-
-#[derive(Default)]
-pub struct Client {
-    client: reqwest::Client,
-}
-
-impl Client {
-    pub fn new() -> Self {
-        Self {
-            client: reqwest::Client::new(),
-        }
-    }
-
-    pub async fn get_json_document<D: serde::de::DeserializeOwned>(
-        &self,
-        url: &str,
-    ) -> JsonResult<JsonDocument<D>> {
-        let res = self
-            .client
-            .get(url)
-            .header(reqwest::header::ACCEPT, "application/vnd.api+json")
-            .send()
-            .await?;
-        let status = res.status();
-
-        if !status.is_success() {
-            return Err(JsonError::InvalidStatus(status));
-        }
-
-        Ok(res.json().await?)
-    }
+/// The library error type
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// Reqwest HTTP error
+    #[error(transparent)]
+    Reqwest(#[from] reqwest::Error),
 }
 
 #[cfg(test)]
@@ -63,7 +25,7 @@ mod tests {
     #[test]
     fn parse_resource_object() {
         let _resource_object: ResourceObject<serde_json::Value> =
-            serde_json::from_str(RESOURCE_OBJECT_JSON).unwrap();
+            serde_json::from_str(RESOURCE_OBJECT_JSON).expect("failed to parse resource object");
         // TODO: Compare values
     }
 }
